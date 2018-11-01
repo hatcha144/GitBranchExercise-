cb0e5183cb5e3c0e1b10edb21c89600d371d490c
Your Name <you@example.com>
2018-11-01 09:25:01 -0400

    Git Branch Exercise

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int userInput = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Your number is {0}", userInput);
+            Console.ReadLine();
+        }
+    }
+}
