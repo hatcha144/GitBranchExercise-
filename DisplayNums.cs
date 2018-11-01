8f8e4918147d2b230f711fec60cbbb65fbed5c9c
Jeremy Ryan <jscott257@gmail.com>
2018-11-01 09:46:46 -0400

    Trying once again

--- DisplayNums.cs
+++ DisplayNums.cs
@@ -8,6 +8,7 @@ namespace GitBranchExercise
 {
     abstract class DisplayNums
     {
+        //this is a method that will display all the ints
         public static void DisplayNum(int x)
         {
             for(int i = 0; i < x; i++)
