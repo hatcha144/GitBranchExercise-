9393804e05157019da3fc4d60bf2b679538148de
Jeremy Ryan <jscott257@gmail.com>
2018-11-01 09:40:40 -0400

    Added DisplayNum method

---
+++ DisplayNums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchExercise
+{
+    abstract class DisplayNums
+    {
+        public static void DisplayNum(int x)
+        {
+            for(int i = 0; i < x; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
